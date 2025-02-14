460cb0ec06b1d58ca06bdb88980b4355de40df41
byLuisfer <luisferdass@gmail.com>
2025-02-14 20:05:43 +0100

    Now tailwind work nice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import viteLogo from '../../client/public/vite.svg';
 import './App.css';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-red-500">Vite + React</h1>
+      <h1 className="text-blue-500 text-9xl">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
