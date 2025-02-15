becb5cac4be117d9856463bba24b962bd212f6aa
byLuisfer <luisferdass@gmail.com>
2025-02-15 19:52:52 +0100

    Update the table Users

--- server/src/db/schema.sql
+++ server/src/db/schema.sql
@@ -11,11 +11,9 @@ FLUSH PRIVILEGES;
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL
 );
-
--- Insert test data
-INSERT INTO users (username, password) VALUES ('user1', 'password123');
 
 -- Confirmation message
 SELECT '✅ DB and Tables right created' AS status;
